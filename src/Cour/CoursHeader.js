82721a9e021a1c32fdde52b2834268d55b1b3318
Rosly <rosly.mokambo@ginov.io>
2023-08-11 14:51:49 +0200

    Introduction a redux toolkit

--- src/Cour/CoursHeader.js
+++ src/Cour/CoursHeader.js
@@ -10,7 +10,7 @@ const CoursHeader = () => {
       <div>
         <h3>React Courses List</h3>
         <p>
-          Cours en favoris : <strong>{undonecours.length}</strong>
+          Les cours que j'aime : <strong>{undonecours.length}</strong>
         </p>
       </div>
       <div>
